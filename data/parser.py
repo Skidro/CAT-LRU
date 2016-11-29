6d424c01881124b0b2d49d1606728ce67cd3dcaa
Waqar Ali <Waqar Ali>
2016-11-29 00:59:00 -0600

    Added files and result for experiment-2. Resolves #2.

--- data/parser.py
+++ data/parser.py
@@ -6,7 +6,7 @@ import pylab as pl
 # Declare the file name
 workingSet = 5120
 partitionSize = '7_5mb'
-fileName = str(workingSet) + '_' + partitionSize + '.log'
+fileName = str(workingSet) + '_' + partitionSize + '_swap.log'
 
 # Verify that the file exists in the current directory
 if not os.path.isfile(os.getcwd() + '/' + fileName):
@@ -70,11 +70,13 @@ def plot_data(fig_name, title, x_axis, data_1, data_2, x_title, y_title):
 	y_axis_lower_bound = min_y - 0.05 * range_y
 	
 	# Create a canvas for drawing the plot
-	fig = pl.figure()
+	fig = pl.figure(figsize = (15, 15))
 
 	# Draw the plots
-	pl.plot(x_axis, data_1, 'r', marker = 'o', label = 'Intensive')
-	pl.plot(x_axis, data_2, 'b', marker = '^', label = 'Mild')
+	pl.plot(x_axis[:43], data_1[:43], 'b', marker = '^', label = 'Mild')
+	pl.plot(x_axis[22:], data_2[22:], 'r', marker = 'o', label = 'Intensive')
+	pl.plot([22, 22], [y_axis_upper_bound, y_axis_lower_bound], 'k--', label = 'Mild-Starts - Intensive Migrates')
+	pl.plot([42, 42], [y_axis_upper_bound, y_axis_lower_bound], 'g--', label = 'Intensive Ends')
 	pl.ylim(y_axis_lower_bound, y_axis_upper_bound)
 	pl.xlim(0, x_axis[-1])
 	pl.title(title)
@@ -82,7 +84,7 @@ def plot_data(fig_name, title, x_axis, data_1, data_2, x_title, y_title):
 	pl.ylabel(y_title)
 
 	# Place a legend on the upper right corner of the plot
-	pl.legend(loc = 'upper right', shadow = True)
+	pl.legend(loc = 'upper left', shadow = True)
 
 	# Save the figure with the given name
 	fig.savefig(fig_name)
@@ -94,4 +96,6 @@ x_axis = range(0, len(data['IPC-1']))
 plot_data('ipc.jpg', 'Instructions per Cycle (IPC)', x_axis, data['IPC-1'], data['IPC-2'], 'Time (seconds)', 'IPC')
 
 # Plot the LLC Miss data
-plot_data('miss.jpg', 'LLC Misses', x_axis, data['MISS-1'], data['MISS-2'], 'Time (seconds)', 'Number of Misses')
+scaled_miss_1 = [x * 1000 for x in data['MISS-1']]
+scaled_miss_2 = [x * 1000 for x in data['MISS-2']]
+plot_data('miss.jpg', 'LLC Misses', x_axis, scaled_miss_1, scaled_miss_2, 'Time (seconds)', 'Number of Misses')
